84e701a82fbf52b75fcaa8d12ed87fc9b6310809
yuliuspratama <yuliuspratama9e@gmail.com>
2022-11-16 02:20:19 +0700

    Penambahan PropType

--- src/Pages/Loginpage.js
+++ src/Pages/Loginpage.js
@@ -4,18 +4,18 @@ import { login } from '../utils/Network-data';
 import Path from '../Components/Path';
 import { useNavigate } from 'react-router-dom';
 import LoginInputUseState from '../Components/LoginInputUseState';
- 
+import PropTypes from 'prop-types';
+
 function LoginPage({ loginSuccess }) {
     const navigate = useNavigate();
     async function onLogin({ email, password }) {
     const { error, data } = await login({ email, password });
-    console.log(data)
+    console.log(data) 
     if (!error) {
       loginSuccess(data);
       navigate(Path.Root);
     }
   }
- 
   return (
     <section className='login-page'>
       <h2>Silahkan Login</h2>
@@ -23,6 +23,10 @@ function LoginPage({ loginSuccess }) {
       <p>Belum punya akun? <Link to={Path.Root}>Daftar di sini.</Link></p>
     </section>
   );
+}
+
+LoginPage.propTypes = {
+  loginSuccess: PropTypes.func.isRequired
 }
  
 export default LoginPage;
